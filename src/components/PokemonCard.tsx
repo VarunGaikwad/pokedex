a203c86e27a960582a119fb9671795e847f3d676
Varun Gaikwad <gaikwadvarun23@gmail.com>
2024-03-25 22:33:31 +0530

    major fix: image lazy loading

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,29 +1,59 @@
+import { useEffect, useState, useRef } from "react";
+import { Link } from "react-router-dom";
 import {
   image_base_url,
   PokemonListType,
   pokemonNumberPadding,
 } from "../data/common";
 import missingno from "../assets/missingno.png";
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+
 export default function PokemonCard({
   entry_number,
   pokemon_species,
 }: PokemonListType) {
-  const [pokeImg, setPokeImg] = useState<string>(missingno);
+  const [pokeImg, setPokeImg] = useState<string>(missingno),
+    cardRef = useRef<HTMLDivElement>(null),
+    [isVisible, setIsVisible] = useState(false),
+    [isImageLoaded, setIsImageLoaded] = useState(false);
 
   useEffect(() => {
-    const fetchImage = new Image();
-    fetchImage.src = `${image_base_url}/other/official-artwork/${entry_number}.png`;
-    fetchImage.onload = () => {
-      setPokeImg(fetchImage.src);
+    if (isVisible) {
+      const fetchImage = new Image();
+      fetchImage.src = `${image_base_url}/other/official-artwork/${entry_number}.png`;
+      fetchImage.onload = () => {
+        setPokeImg(fetchImage.src);
+        setIsImageLoaded(true);
+      };
+      return;
+    }
+
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsVisible(entry.isIntersecting);
+      },
+      {
+        threshold: 0.5,
+      }
+    );
+
+    if (cardRef.current) {
+      observer.observe(cardRef.current);
+    }
+
+    return () => {
+      if (cardRef.current) {
+        observer.unobserve(cardRef.current);
+      }
     };
-  }, [entry_number]);
+  }, [entry_number, isVisible]);
 
   return (
     <Link to={`/pokedex/${entry_number}`}>
       <div
-        className="h-max bg-slate-100 rounded-md p-2 flex flex-col"
+        ref={cardRef}
+        className={`h-max bg-slate-100 rounded-md p-2 flex flex-col ${
+          isVisible ? "opacity-100 translate-y-0" : "opacity-0 -translate-y-4"
+        } transition-opacity duration-500 ease-in-out`}
         style={{
           boxShadow:
             "0px 0px 10px 5px rgba(0,0,0,0.1),inset 0px 3.5rem 5px 0px white",
@@ -35,7 +65,7 @@ export default function PokemonCard({
         <img
           height={20}
           loading="lazy"
-          className="m-auto"
+          className={`m-auto ${isImageLoaded ? "visible" : "invisible"}`}
           src={pokeImg}
           alt={pokemon_species.name}
         />
