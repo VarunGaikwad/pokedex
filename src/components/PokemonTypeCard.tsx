c50cc9ae3451c954c8e55b4670f9fa484162afdf
Varun Gaikwad <gaikwadvarun23@gmail.com>
2024-03-25 20:38:28 +0530

    Updates

--- src/components/PokemonTypeCard.tsx
+++
@@ -1,9 +0,0 @@
-export default function PokemonTypeCard({ children }: { children: string }) {
-  return (
-    <div
-      className={`bg-${children} text-sm text-white p-1 w-32 text-center rounded-full capitalize font-semibold`}
-    >
-      {children}
-    </div>
-  );
-}
