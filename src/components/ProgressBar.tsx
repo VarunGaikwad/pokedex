b69c8ae762bc411e90c5f35a433c8382d1586339
Varun Gaikwad <gaikwadvarun23@gmail.com>
2024-03-27 08:40:18 +0530

    Progress bar animation

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,10 +1,24 @@
-export default function ProgressBar({ percentage }: { percentage: number }) {
+import { useEffect, useState } from "react";
+
+export default function ProgressBar({
+  percentage = 0,
+}: {
+  percentage: number;
+}) {
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setValue(percentage);
+    }, 0);
+  }, [percentage]);
+
   return (
     <div className="flex-grow h-4 w-full bg-gray-200 rounded-full overflow-hidden">
       <div
         className="h-full bg-current shadow-none rounded-r-full"
         style={{
-          width: `${percentage}%`,
+          width: `${value}%`,
           transition: "width 2s, background-color 1s",
         }}
       ></div>
